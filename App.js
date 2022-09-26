bb9a51dae4a1c415592165e36a072a75794d7644
Nicole <nicoleno@kth.se>
2022-09-26 18:03:53 +0200

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TextInput} from 'react-native';
+
+//för att köra: npm start
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Text>DrinkZ</Text>
+        <StatusBar style="auto" />
+        <Image style={styles.image}source={require('./assets/images/shaker.jpg')} />
+        <Button title="Shake me" />
+        <TextInput style= {styles.searchBar} placeholder="Search for ingredients..." />
+      </View>
+
   );
 }
 
@@ -16,5 +22,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  searchBar: {
+    color: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth : 2,
+    borderColor: 'blue',
+    padding: 15,
+    borderRadius: 10
+  },
+
+  image: {
+    width:150,
+    height: 150,
+    margin: 20
   },
 });
