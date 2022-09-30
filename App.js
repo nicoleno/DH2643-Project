292c8c9cb07453331d43a7f3c31ab69a5bf63973
Nicole <66645928+nicoleno@users.noreply.github.com>
2022-10-01 00:04:19 +0200

    Search function

--- App.js
+++ App.js
@@ -1,18 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Image, Button, TextInput} from 'react-native';
 
-//för att köra: npm start
+const data = require("./data.json")
+//console.log(data[0].name); 
+const results = []
+
+for (let i = 0; i < data.length; i++) {
+  results.push(data[i].name)
+}
 
 export default function App() {
-  return (
-      <View style={styles.container}>
-        <Text>DrinkZ</Text>
-        <StatusBar style="auto" />
-        <Image style={styles.image}source={require('./assets/images/shaker.jpg')} />
-        <Button title="Shake me" />
-        <TextInput style= {styles.searchBar} placeholder="Search for ingredients..." />
-      </View>
+  const [search, setSearch] = useState("");
+  const [matches, setMatches] = useState([]);
 
+  function searchDrink (list, substring) {
+    const newMatches = []
+    for (let i = 0; i < list.length; i++) {
+      list.find(element => {
+        if (element.includes(substring) && (newMatches.includes(element)=== false) ){
+          newMatches.push(element)}})
+        }
+        if (newMatches.length == 0){
+          newMatches.push("No drinks were found...")
+        }
+        setMatches(newMatches)
+    //TODO: skriv om med concat
+    }
+  
+  return (
+    <View style={styles.container}>   
+      <TextInput style={styles.searchBar} placeholder="Search drink..." 
+        onChangeText = {(search)=> setSearch(search)} editable={true}/>
+      <Button title="Search" onPress={() => searchDrink (results, search)}></Button>
+      {matches.map((match) => <Text key={match}>{match}</Text>)}
+      <Image style={styles.image}source={require('./assets/images/shaker.jpg')} />
+    </View>
   );
 }
 
@@ -25,11 +47,11 @@ const styles = StyleSheet.create({
   },
 
   searchBar: {
-    color: '#fff',
+    color: '#666',
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth : 2,
-    borderColor: 'blue',
+    borderColor: 'black',
     padding: 15,
     borderRadius: 10
   },
