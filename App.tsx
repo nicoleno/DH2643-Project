519d7ea564a7ded97282a84035540d8a136da147
Julle-96 <juliusalbiz@hotmail.com>
2022-10-06 10:30:29 +0200

    feat: implement drag and drop + overall layout, typescript :)

---
+++ App.tsx
@@ -0,0 +1,143 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+import DraggableCard from './components/dndCards';
+
+//för att köra: npm start
+
+export default function App() {
+
+  // TO DO - Set state för att ändra färgen på knappen beroende på vilken knapp är intryckt. 
+  let shaker = {
+    height: 0,
+    width: 0,
+    posX: 0,
+    posY: 0,
+  }
+
+  function getShakerPos(height: number, width: number, x: number, y: number) {
+    console.log("hej");
+
+    shaker.height = height;
+    console.log(shaker.height);
+    shaker.width = width;
+    shaker.posX = x;
+    shaker.posY = y;
+  }
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <View style={styles.navbuttons}>
+          <TouchableOpacity onPress={() => { alert("you clicked me") }}>
+            <Image style={styles.image2} source={require('./assets/images/menu.png')} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => { alert("you clicked me") }}>
+            <Image style={styles.image2} source={require('./assets/images/settings.png')} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.shakerArea}>
+          <StatusBar style="auto" />
+
+          <Image style={styles.image} source={require('./assets/images/shaker.png')} onLayout={(event) => {
+            const layout = event.nativeEvent.layout;
+            console.log("height:", layout.height);
+            console.log("width:", layout.width);
+            console.log("x:", layout.x);
+            console.log("y:", layout.y);
+            getShakerPos(layout.height, layout.width, layout.x, layout.y);
+          }}
+          />
+        </View>
+        <View style={styles.bottomBar}>
+          <View style={styles.header}>
+            <View style={styles.toggle}>
+              <TouchableOpacity onPress={() => { alert("you clicked me") }}>
+                <Image style={styles.image2} source={require('./assets/images/lemon.png')} />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => { alert("you clicked me") }}>
+                <Image style={styles.image2} source={require('./assets/images/bottle.png')} />
+              </TouchableOpacity>
+            </View>
+            <Text>här har vi vår search bar</Text>
+          </View>
+          <View style={styles.cardContainer}>
+            <DraggableCard />
+            <DraggableCard />
+          </View>
+        </View>
+      </View></GestureHandlerRootView >
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'flex',
+  },
+
+  navbuttons: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  searchBar: {
+    color: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 2,
+    borderColor: 'blue',
+    padding: 15,
+    borderRadius: 10
+  },
+
+  image: {
+    width: 150,
+    height: 150,
+    margin: 20
+  },
+  shakerArea: {
+    flex: 1,
+    height: 300,
+    marginTop: 100,
+
+  },
+
+  bottomBar: {
+    flex: 2,
+    alignSelf: 'stretch',
+    backgroundColor: 'rgb(182,227,136)',
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
+    marginTop: 200,
+    justifyContent: 'space-between',
+  },
+  header: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  cardContainer: {
+    backgroundColor: 'rgb(199, 242, 164)',
+    flexDirection: 'row',
+    height: 200,
+  },
+  toggle: {
+    flexDirection: 'row',
+
+  },
+  image2: {
+    height: 50,
+    width: 50,
+    borderRadius: 30,
+    margin: 5,
+    backgroundColor: 'rgb(199, 242, 164)',
+  }
+});
---
+++ components/dndCards.tsx
@@ -0,0 +1,75 @@
+import { GestureHandlerRootView, PanGestureHandler, PanGestureHandlerGestureEvent } from 'react-native-gesture-handler';
+import Animated, { useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
+import React from 'react';
+import { StyleSheet, Text, View, Image, Button, TextInput } from 'react-native';
+
+type ContextType = {
+  translateX: number;
+  translateY: number;
+}
+const DraggableCard = () => {
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+
+  const panGestureEvent = useAnimatedGestureHandler
+    <PanGestureHandlerGestureEvent, ContextType>
+    ({
+      onStart: (event, context) => {
+        context.translateX = translateX.value;
+        context.translateY = translateY.value;
+      },
+      onActive: (event, context) => {
+        translateX.value = event.translationX + context.translateX
+        translateY.value = event.translationY + context.translateY
+
+      },
+      onEnd: (event, context) => {
+
+        // NÄSTA steg, hitta koordinater av shakern och gör en if sats.
+        if ((110 < event.absoluteX) && (event.absoluteX < 300)
+          && (298 < event.absoluteY) && (event.absoluteY < 470)) {
+
+          console.log("inuti shakern")
+        }
+
+        else {
+          translateX.value = withSpring(0);
+          translateY.value = withSpring(0);
+        }
+
+
+      },
+    })
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        }
+      ]
+    }
+  })
+  return (
+    <PanGestureHandler onGestureEvent={panGestureEvent}>
+      <Animated.View style={[styles.square, rStyle]} />
+
+    </PanGestureHandler>)
+
+}
+
+const styles = StyleSheet.create({
+  square: {
+    width: 100,
+    height: 150,
+    backgroundColor: 'rgb(225, 255, 177)',
+    borderRadius: 20,
+    margin: 10,
+  }
+})
+
+export default DraggableCard
