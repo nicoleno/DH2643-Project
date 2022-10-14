84cebc8ba0da1db8ca097964a5b5d7a6d82a2768
Nicole <66645928+nicoleno@users.noreply.github.com>
2022-10-14 16:37:18 +0200

    ?

--- DrinkList.tsx
+++ DrinkList.tsx
@@ -1,79 +1,63 @@
-import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, Image } from 'react-native';
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-      display: 'flex',
-    },
-  
-    navbuttons: {
-      margin: 20,
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-    },
-  
-    searchBar: {
-      color: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderWidth: 2,
-      borderColor: 'blue',
-      padding: 15,
-      borderRadius: 10
-    },
-  
-    image: {
-      width: 150,
-      height: 150,
-      margin: 20
-    },
-    shakerArea: {
-      flex: 1,
-      height: 300,
-      marginTop: 100,
-  
-    },
-  
-    bottomBar: {
-      flex: 2,
-      alignSelf: 'stretch',
-      backgroundColor: 'rgb(182,227,136)',
-      borderTopRightRadius: 30,
-      borderTopLeftRadius: 30,
-      marginTop: 200,
-      justifyContent: 'space-between',
-    },
-    header: {
-      margin: 20,
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-    },
-    cardContainer: {
-      backgroundColor: 'rgb(199, 242, 164)',
-      flexDirection: 'row',
-      height: 200,
-    },
-    toggle: {
-      flexDirection: 'row',
-  
-    },
-    image2: {
-      height: 50,
-      width: 50,
-      borderRadius: 30,
-      margin: 5,
-      backgroundColor: 'rgb(199, 242, 164)',
-    }
-  });
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, Image, Button } from 'react-native';
+import { Hamburger } from './components/menuButton';
+import Animated from 'react-native-reanimated';
+import React from 'react';
+import GestureFlipView from 'react-native-gesture-flip-card';
 
-const DrinkList = () => {
+const DrinkList = ({ navigation }) => {
+    const scrollX = React.useRef(new Animated.Value(0)).current;
+
+    const Data = [...(Array(30).keys())].map(i => ({ label: `${i + 1}  out of 30`, value: `${i + 1} ` }));
+    const card_width = 300;
+    const card_height = 500;
 
     return (
-        <View>
-            <Text>Hej här e ett recept</Text>
-        </View>
+        <View style={{ flex: 1, backgroundColor: '#fff', }}>
+            <Hamburger navigation={navigation} />
+            <Animated.FlatList
+                contentContainerStyle={{ alignItems: 'center', paddingHorizontal: 50 }}
+                horizontal data={Data}
+                showsHorizontalScrollIndicator={false}
+                onScroll={Animated.event(
+                    [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+                    { useNativeDriver: true }
+                )}
+                keyExtractor={item => item.value}
+                renderItem={({ item, index }) => {
+
+                    const inputRange = [
+                        card_width * (index - 1), card_width * index, card_width * (index + 1)
+                    ]
+
+                    const scale = scrollX.interpolate({
+                        inputRange, outputRange: [0.75, 1, 0.75]
+                    })
+                    return <Animated.View style={{ transform: [{ scale }] }}>
+                        <TouchableOpacity style={{
+                            justifyContent: 'center', alignItems: 'center', height: card_height, width: card_width,
+                            backgroundColor: '#F8EDE3', borderRadius: 30,
+                            shadowColor: 'black',
+                            elevation: 10,
+                            shadowOpacity: 1,
+                            shadowRadius: 30
+                        }} onPress={() => { navigation.navigate("Details") }}>
+
+                            <Image style={{ height: 50, width: 50, }} source={require('./assets/images/lemon.png')} />
+                            <Text> These Ingredients you have: {"haaay"}</Text>
+                            <Text> These Ingredients you don't have: {"haay"}</Text>
+                            <Text >{item.label}</Text>
+                            <Text>Click to see more bruh</Text></TouchableOpacity>
+                    </Animated.View>
+
+
+                }}>
+            </Animated.FlatList>
+            <View style={{ alignItems: 'center', }}>
+                <TouchableOpacity style={{ backgroundColor: 'gray', height: 50, width: 200, borderRadius: 30, justifyContent: 'center', marginBottom: 100, }}
+                    onPress={() => navigation.navigate('Home')}>
+                    <Text style={{ textAlign: 'center' }}>Back to Shaker</Text>
+                </TouchableOpacity></View>
+        </View >
     )
 }
 
