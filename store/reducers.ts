b5ff6552fcb29297a2ec3c59fd08ddb859ad1fee
Lisa Balzar <lisamariebalzar@gmail.com>
2022-10-14 13:07:00 +0200

    feat: alcohol ingredients and a search matched ingredients added to store

--- store/reducers.ts
+++ store/reducers.ts
@@ -1,8 +1,13 @@
 import ingredientsReducer from "./reducers/ingredientsReducer";
+import alcoholReducer from "./reducers/alcoholReducer";
+import searchedItemReducer from "./reducers/searchedItemReducer";
 import { combineReducers } from "redux";
 
 const reducers = combineReducers({
     ingredients: ingredientsReducer,
+    alcohols: alcoholReducer,
+    matched: searchedItemReducer,
+    
   });
   
 export default reducers;
