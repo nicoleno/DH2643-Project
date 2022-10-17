1101893a77973e49c3a8c7d5da9e37636b182766
Nicole <66645928+nicoleno@users.noreply.github.com>
2022-10-17 12:43:19 +0200

    design

--- HomePage.tsx
+++ HomePage.tsx
@@ -2,149 +2,129 @@ import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import DraggableCard from './components/dndCards';
-import { StyleSheet, Text, View, Image, TouchableOpacity, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Button, Dimensions } from 'react-native';
 import ShakerModel from './models/shaker';
 import SearchDrink from './search';
-import { useNavigate } from "react-router-dom";
-// let navigate = useNavigate(); 
-// const routeChange = () =>{ 
-//   let path = `newPath`; 
-//   navigate(path);
-// }
+import { useFonts } from '@expo-google-fonts/carter-one';
+
 
 export const HomePage = () => {
     const shaker = new ShakerModel;
-      // TO DO - Set state för att ändra färgen på knappen beroende på vilken knapp är intryckt. 
-  // const shaker = {
-  //   height: 0,
-  //   width: 0,
-  //   posX: 0,
-  //   posY: 0,
-  // }
-
-  // function getShakerPos(height: number, width: number, x: number, y: number) {
-  //   console.log("hej");
-
-  //   shaker.height = height;
-  //   console.log(shaker.height);
-  //   shaker.width = width;
-  //   shaker.posX = x;
-  //   shaker.posY = y;
-  // }
+    const [loaded] = useFonts({
+      //Poppins: require('./assets/fonts/Poppins-Black.ttf'),
+      Carter: require('./assets/fonts/CarterOne-Regular.ttf')
+    });
     
     return (
-        <GestureHandlerRootView style={{ flex: 1 }}>
-            <View style={styles.container}>
-                <View style={styles.navbuttons}>
-                <TouchableOpacity onPress={() => { alert("you clicked me") }}>
-                    <Image style={styles.image2} source={require('./assets/images/menu.png')} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => { alert("you clicked me") }}>
-                    <Image style={styles.image2} source={require('./assets/images/settings.png')} />
-                </TouchableOpacity>
-                </View>
-                <SearchDrink/>
-                {/* <button onClick={routeChange}>
-              Login
-            </button> */}
-                <View style={styles.shakerArea}>
-                <StatusBar style="auto" />
-
-                <Image style={styles.image} source={require('./assets/images/shaker.png')} onLayout={(event) => {
-                    const layout = event.nativeEvent.layout;
-                    shaker.setHeight(layout.height);
-                    shaker.setWidth(layout.width);
-                    shaker.setPosX(layout.x);
-                    shaker.setPosY(layout.y);
-                }}
-                />
-                </View>
-                <View style={styles.bottomBar}>
-                <View style={styles.header}>
-                    <View style={styles.toggle}>
-                    <TouchableOpacity onPress={() => { alert("you clicked me") }}>
-                        <Image style={styles.image2} source={require('./assets/images/lemon.png')} />
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => { alert("you clicked me") }}>
-                        <Image style={styles.image2} source={require('./assets/images/bottle.png')} />
-                    </TouchableOpacity>
-                    </View>
-                    <Text>här har vi vår search bar</Text>
-                </View>
-                <View style={styles.cardContainer}>
-                    <DraggableCard />
-                    <DraggableCard />
-                </View>
-                </View>
-            </View>
-        </GestureHandlerRootView >
-)};
+      
+    <View style= {styles.HomePage}>
+      <Image style={styles.background} source={require('./assets/images/background.png')} />
+      <Image style={styles.shaker} source={require('./assets/images/shaker.png')} />
+      <View style={styles.Rectangle_36}></View>
+      <View style={styles.logo}></View>
+      <View style={styles.ellipse}></View>
+      <View><Text style={styles.shakeit} >ShakeIt</Text></View>
+    </View>
+    
+)
+}
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-      display: 'flex',
+
+    HomePage:{
+      position: "relative",
+      width: Dimensions.get("window").width,
+      height: 847,
+      overflow: "hidden",
+      backgroundColor: "rgba(255, 255, 255, 1)",
+      left: 0,
+      top: 0,
     },
-  
-    navbuttons: {
-      margin: 20,
-      flexDirection: 'row',
-      justifyContent: 'space-between',
+
+    background:{
+      position: "absolute",
+      width: 675,
+      height: 539,
+      opacity: 1,
+      left: -131,
+      right: "auto",
+      top: -9,
+      botton: "auto",
+      backgroundColor: "rgba(0, 0, 0, 0)"
     },
-  
-    searchBar: {
-      color: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderWidth: 2,
-      borderColor: 'blue',
-      padding: 15,
-      borderRadius: 10
+    
+    shaker:{
+      position: "absolute",
+ 
+      opacity: 1,
+      left: 21,
+      right: "auto",
+      top: 127,
+      bottom: "auto",
+      shadowColor: "rgb(0,0,0)",
+      textShadowOffset: {
+        width: 0,
+        height: 4
+      },
+      shadowOpacity: 0.25,
+      shadowRadius:4,
+      backgroundColor: "rgba(0,0,0,0)"
+
     },
-  
-    image: {
-      width: 150,
-      height: 150,
-      margin: 20
+
+    Rectangle_36: {
+      position: "absolute",
+      width: 392,
+      heigh: 326,
+      borderRadius: 0,
+      opacity: 1,
+      backgroundColor: "black"
     },
-    shakerArea: {
-      flex: 1,
-      height: 300,
-      marginTop: 100,
-  
+
+    logo:{
+      position: "absolute",
+      width: 136,
+      height: 49,
+      opacity: 1,
+      left: 127,
+      right: "auto",
+      top: 77,
+      bottom: "auto",
+      backgroundColor: "rgba(0,0,0,0)"
     },
-  
-    bottomBar: {
-      flex: 2,
-      alignSelf: 'stretch',
-      backgroundColor: 'rgb(182,227,136)',
-      borderTopRightRadius: 30,
-      borderTopLeftRadius: 30,
-      marginTop: 200,
-      justifyContent: 'space-between',
+
+    ellipse:{
+      position: "absolute",
+      width: 58,
+      heigh: 58,
+      backgroundColor: "rgba(255, 255, 255, 0.9200000166893005",
+      borderwidth: 1,
+      borderstyle: "solid",
+      left: 169,
+      right: "auto",
+      top: 293,
+      bottom: "auto"
     },
-    header: {
-      margin: 20,
-      flexDirection: 'row',
-      justifyContent: 'space-between',
+
+    hamburger:{
+      position: "absolute",
+      width: 21,
+      height: 21,
+      left: 326,
+      right: "auto",
+      top: 81,
+      bottom: "auto",
+      backgroundColor: "rgba(0,0,0,0)"
     },
-    cardContainer: {
-      backgroundColor: 'rgb(199, 242, 164)',
-      flexDirection: 'row',
-      height: 200,
+
+    shakeit : {
+      fontFamily: "Carter",
+      textAlign: "center",
+      top: 40,
+      fontSize: 30,
+      color: "rgba(255,255,255,1)",
+    
     },
-    toggle: {
-      flexDirection: 'row',
-  
-    },
-    image2: {
-      height: 50,
-      width: 50,
-      borderRadius: 30,
-      margin: 5,
-      backgroundColor: 'rgb(199, 242, 164)',
-    }
   });
+
+  
