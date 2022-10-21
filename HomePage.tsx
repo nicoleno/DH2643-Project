59c5771826325d1795034978d0d9fc15a8abc0ae
Lisa Balzar <lisamariebalzar@gmail.com>
2022-10-21 12:14:02 +0200

    fix

--- HomePage.tsx
+++ HomePage.tsx
@@ -17,243 +17,227 @@ import { RootState } from './store/reducers';
 import alcohol from './assets/alcohol.json';
 
 export const HomePage = ({ navigation }) => {
+    const [drinks, setDrinks] = React.useState([]);
 
-    let shaker = new ShakerModel;
+    useEffect(() => {
+        fetch("http://130.229.145.164:3000/drinks", {
+            method: "get",
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+        }).then((res) => res.json()).then((data) => setDrinks(data.drinks)).catch((err) => err);
+    }, []);
     const [loaded] = useFonts({
         Poppins: require('./assets/fonts/Poppins-Regular.ttf'),
         Carter: require('./assets/fonts/CarterOne-Regular.ttf')
     });
 
+    // console.log(store.getState());
+    let shaker = new ShakerModel;
+    // const dispatch = useDispatch();
+    // dispatch(matchedItems(ingredients));
+
+    // const ingredientsToShow = useSelector((state: RootState) => state.matched);
+    const [ingredientButtonEnabled, setIngredientButtonEnabled] = useState(true);
+    const [alcoholButtonEnabled, setAlcoholButtonEnabled] = useState(false);
+
+    const handleIngredientButtonPressed = () => {
+        setIngredientButtonEnabled(true);
+        setAlcoholButtonEnabled(false);
+        console.log("ingredient button pressed!")
+    }
+    const handleAlcoholButtonPressed = () => {
+        setIngredientButtonEnabled(false);
+        setAlcoholButtonEnabled(true);
+        console.log("alcohol button pressed!")
+    }
+
+    ShakeEventExpo.addListener(() => {
+        navigation.navigate('DrinkList');
+    })
 
 
-    export const HomePage = ({ navigation }) => {
-        const [drinks, setDrinks] = React.useState([]);
-
-        useEffect(() => {
-            fetch("http://130.229.145.164:3000/drinks", {
-                method: "get",
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json'
-                },
-            }).then((res) => res.json()).then((data) => setDrinks(data.drinks)).catch((err) => err);
-        }, []);
-
-        // console.log(store.getState());
-        let shaker = new ShakerModel;
-        // const dispatch = useDispatch();
-        // dispatch(matchedItems(ingredients));
-
-        // const ingredientsToShow = useSelector((state: RootState) => state.matched);
-        const [ingredientButtonEnabled, setIngredientButtonEnabled] = useState(true);
-        const [alcoholButtonEnabled, setAlcoholButtonEnabled] = useState(false);
-
-        const handleIngredientButtonPressed = () => {
-            setIngredientButtonEnabled(true);
-            setAlcoholButtonEnabled(false);
-            console.log("ingredient button pressed!")
-        }
-        const handleAlcoholButtonPressed = () => {
-            setIngredientButtonEnabled(false);
-            setAlcoholButtonEnabled(true);
-            console.log("alcohol button pressed!")
-        }
-
-        ShakeEventExpo.addListener(() => {
-            navigation.navigate('DrinkList');
-        })
-
-
-        const Item = ({ ingredient }) => {
-            return (
-                <View style={styles.cardContainer}>
-                    <DraggableCard ingredient={ingredient} />
-                </View>
-            );
-        }
-        const renderItem = ({ item }) => (
-            <Item ingredient={item} />
-        );
-
+    const Item = ({ ingredient }) => {
         return (
-            <GestureHandlerRootView style={styles.background}>
-                <LinearGradient start={{ x: 0.0, y: 0.55 }} end={{ x: 0.5, y: 1.0 }} colors={['#414141', '#171717']} style={styles.background}>
+            <View style={styles.cardContainer}>
+                <DraggableCard ingredient={ingredient} />
+            </View>
+        );
+    }
+    const renderItem = ({ item }) => (
+        <Item ingredient={item} />
+    );
 
-                    <ImageBackground style={styles.image3} source={require('./assets/images/table.png')}>
-                        <Text style={styles.shakeit} >Shakeit</Text><Hamburger navigation={navigation} />
-                        <View style={styles.shakerArea}>
-                            <View style={styles.topsection} >
-                                <IngredientCountIcon />
-                                <Image style={styles.shakerReal} source={require('./assets/images/shaker-real.png')}
-                                    onLayout={(event) => {
-                                        const layout = event.nativeEvent.layout;
-                                        console.log("layout", layout);
-                                        shaker.setHeight(layout.height);
-                                        shaker.setWidth(layout.width);
-                                        shaker.setPosX(layout.x);
-                                        shaker.setPosY(layout.y);
-                                    }} />
-                            </View>
-                            <Image style={styles.image4} source={require('./assets/images/shaketomix.png')}></Image>
+    return (
+        <GestureHandlerRootView style={styles.background}>
+            <LinearGradient start={{ x: 0.0, y: 0.55 }} end={{ x: 0.5, y: 1.0 }} colors={['#414141', '#171717']} style={styles.background}>
+
+                <ImageBackground style={styles.image3} source={require('./assets/images/table.png')}>
+                    <Text style={styles.shakeit} >Shakeit</Text><Hamburger navigation={navigation} />
+                    <View style={styles.shakerArea}>
+                        <View style={styles.topsection} >
+                            <IngredientCountIcon />
+                            <Image style={styles.shakerReal} source={require('./assets/images/shaker-real.png')}
+                                onLayout={(event) => {
+                                    const layout = event.nativeEvent.layout;
+                                    console.log("layout", layout);
+                                    shaker.setHeight(layout.height);
+                                    shaker.setWidth(layout.width);
+                                    shaker.setPosX(layout.x);
+                                    shaker.setPosY(layout.y);
+                                }} />
                         </View>
-                    </ImageBackground>
+                        <Image style={styles.image4} source={require('./assets/images/shaketomix.png')}></Image>
+                    </View>
+                </ImageBackground>
 
-                    <View style={styles.bottomBar}>
-                        <Text style={styles.poppins} >Add items</Text>
-                        <Text style={styles.poppins2} >What items do you have at home? Drag and drop to  the shaker!</Text>
-                        <View style={styles.header}>
-                            <ToggleComponent />
-                            <SearchIngredient />
-                        </View >
-                        <FlatList style={{ overflow: "visible" }} horizontal data={ingredientButtonEnabled ? ingredients : alcohol} renderItem={renderItem} keyExtractor={item => item.id} />
+                <View style={styles.bottomBar}>
+                    <Text style={styles.poppins} >Add items</Text>
+                    <Text style={styles.poppins2} >What items do you have at home? Drag and drop to  the shaker!</Text>
+                    <View style={styles.header}>
+                        <ToggleComponent />
+                        <SearchIngredient />
                     </View >
-                </LinearGradient >
-            </GestureHandlerRootView >
+                    <FlatList style={{ overflow: "visible" }} horizontal data={ingredientButtonEnabled ? ingredients : alcohol} renderItem={renderItem} keyExtractor={item => item.id} />
+                </View >
+            </LinearGradient >
+        </GestureHandlerRootView >
 
-        )
-    };
+    )
+};
 
-    const styles = StyleSheet.create({
-
-
-        background: {
-            flex: 1,
-            color: "rgba(255,255,1,1)"
-        },
-
-        topsection: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'column'
-
-        },
-
-        topsection2: {
-            alignContent: 'flex-end',
-        },
-
-        container: {
-            flex: 1,
-            backgroundColor: '#fff',
-            alignItems: 'center',
-            justifyContent: 'center',
-            display: 'flex',
-        },
-
-        navbuttons: {
-            margin: 10,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-        },
-
-        searchBar: {
-            color: '#fff',
-            alignItems: 'center',
-            justifyContent: 'center',
-            borderWidth: 2,
-            borderColor: 'blue',
-            borderRadius: 10
-        },
-
-        image: {
-            width: 200,
-            height: 200,
-            margin: 20,
-        },
-
-        shakerArea: {
-            flex: 1,
-        },
-
-        bottomBar: {
-            flex: 2,
-            alignSelf: 'stretch',
-
-            justifyContent: 'space-between',
-        },
-        header: {
-            marginTop: 25,
-            marginLeft: 20,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-        },
-        cardContainer: {
-            backgroundColor: '#414141',
-            flexDirection: 'row',
-            height: 200,
-        },
-        toggle: {
-            flexDirection: 'row',
-
-        },
-        image2: {
-            height: 50,
-            width: 50,
-            borderRadius: 30,
-            margin: 5,
-            backgroundColor: 'rgb(199, 242, 164)',
-        },
-
-<<<<<<< HEAD
-        image3: {
-            height: 450,
-
-        },
-
-        image4: {
-            height: 80,
-            width: 440,
-            alignSelf: 'center'
-
-        },
-
-        shakeit: {
-            fontFamily: "Carter",
-            textAlign: "center",
-            flexDirection: 'row',
-            top: 40,
-            fontSize: 30,
-            color: "rgba(255,255,255,1)",
-
-        },
-
-        poppins: {
-            fontFamily: "Poppins",
-            weight: "Light",
-            textAlign: "left",
-            marginLeft: 25,
-            flexDirection: 'row',
-            top: 40,
-            fontSize: 20,
-            color: "rgba(255, 255, 255,1)",
-
-        },
+const styles = StyleSheet.create({
 
 
-        poppins2: {
-            fontFamily: "Poppins",
-            textAlign: "left",
-            marginLeft: 25,
-            flexDirection: 'row',
-            top: 40,
-            fontSize: 10,
-            color: "rgba(255, 255, 255,1)",
+    background: {
+        flex: 1,
+        color: "rgba(255,255,1,1)"
+    },
 
-        },
+    topsection: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column'
 
-        shakerReal: {
-            height: 300,
-            width: 230,
-            alignSelf: 'center',
-        },
-=======
-    image2NotChosen: {
+    },
+
+    topsection2: {
+        alignContent: 'flex-end',
+    },
+
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        display: 'flex',
+    },
+
+    navbuttons: {
+        margin: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+
+    searchBar: {
+        color: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 2,
+        borderColor: 'blue',
+        borderRadius: 10
+    },
+
+    image: {
+        width: 200,
+        height: 200,
+        margin: 20,
+    },
+
+    shakerArea: {
+        flex: 1,
+    },
+
+    bottomBar: {
+        flex: 2,
+        alignSelf: 'stretch',
+
+        justifyContent: 'space-between',
+    },
+    header: {
+        marginTop: 25,
+        marginLeft: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    cardContainer: {
+        backgroundColor: '#414141',
+        flexDirection: 'row',
+        height: 200,
+    },
+    toggle: {
+        flexDirection: 'row',
+
+    },
+    image2: {
         height: 50,
         width: 50,
         borderRadius: 30,
         margin: 5,
-        backgroundColor: 'gray',
-    }
->>>>>>> master
-    });
+        backgroundColor: 'rgb(199, 242, 164)',
+    },
+
+    image3: {
+        height: 450,
+
+    },
+
+    image4: {
+        height: 80,
+        width: 440,
+        alignSelf: 'center'
+
+    },
+
+    shakeit: {
+        fontFamily: "Carter",
+        textAlign: "center",
+        flexDirection: 'row',
+        top: 40,
+        fontSize: 30,
+        color: "rgba(255,255,255,1)",
+
+    },
+
+    poppins: {
+        fontFamily: "Poppins",
+        weight: "Light",
+        textAlign: "left",
+        marginLeft: 25,
+        flexDirection: 'row',
+        top: 40,
+        fontSize: 20,
+        color: "rgba(255, 255, 255,1)",
+
+    },
+
+
+    poppins2: {
+        fontFamily: "Poppins",
+        textAlign: "left",
+        marginLeft: 25,
+        flexDirection: 'row',
+        top: 40,
+        fontSize: 10,
+        color: "rgba(255, 255, 255,1)",
+
+    },
+
+    shakerReal: {
+        height: 300,
+        width: 230,
+        alignSelf: 'center',
+    },
+});
